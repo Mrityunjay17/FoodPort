ca7a49a3db23b6de901506b8f2f55ee74c46cf96
Mrityunjay <17mrityunjaypandey@gmail.com>
2018-01-22 23:35:49 +0530

    //login page is working

--- app/src/main/java/com/example/a17mri/foodport/Login.java
+++ app/src/main/java/com/example/a17mri/foodport/Login.java
@@ -1,13 +1,105 @@
 package com.example.a17mri.foodport;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class Login extends AppCompatActivity  {
+    private AppCompatEditText emilAddress,password;
+    private FirebaseAuth mAuth;
+    boolean success=false;
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mAuth = FirebaseAuth.getInstance();
+        emilAddress=findViewById(R.id.emailAddress);
+        password=findViewById(R.id.password);
+        login=findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(password.getWindowToken(),
+                            InputMethodManager.RESULT_UNCHANGED_SHOWN);
+                }
+
+                if(login(emilAddress.getText().toString(),password.getText().toString())){
+                    Intent intent=new Intent(getApplicationContext(),Home.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId()==android.R.id.home){
+            back();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        back();
+        super.onBackPressed();
+    }
+
+    private void back(){
+        Intent intent=new Intent(getApplicationContext(),Main.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    private boolean login(String email,String password){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                           success=true;
+                        } else {
+                            String message= task.getException().getMessage();
+                            // If sign in fails, display a message to the user.
+                            Snackbar bar=Snackbar.make(login.getRootView(),message,Snackbar.LENGTH_SHORT);
+                            bar.show();
+                        }
+
+                        // ...
+                    }
+                });
+        return success;
+    }
+
+
 }
--- app/src/main/java/com/example/a17mri/foodport/Main.java
+++ app/src/main/java/com/example/a17mri/foodport/Main.java
@@ -1,13 +1,53 @@
 package com.example.a17mri.foodport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Main extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class Main extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login=findViewById(R.id.login);
+        login.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.login){
+            move(Login.class);
+        }
+    }
+
+    public void move(Class Main){
+        Intent change=new Intent(this,Main);
+        startActivity(change);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        super.onBackPressed();
+    }
+
+
+    @Override
+    protected void onStart() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null){
+            Intent intent=new Intent(this,Home.class);
+            startActivity(intent);
+            finish();
+        }
+        super.onStart();
     }
 }
